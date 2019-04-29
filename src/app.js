aff7fe509ec7665bf58acdc7cbb6041b4d1409c2
samisaga95 <santoshkumaramisagadda@Santoshkumars-MacBook-Pro.local>
2019-04-28 23:35:50 -0700

    Pushed

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./Components/IndecisionApp";
+import "normalize.css/normalize.css";
+import "./Styles/styles.scss";
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
